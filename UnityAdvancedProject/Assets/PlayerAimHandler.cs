1c536a2c075d31de0c9e7fd4c4c0e322d8a02c42
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-02-12 20:04:00 +0200

    .

---
+++ UnityAdvancedProject/Assets/PlayerAimHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimHandler : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundLayer = 1;
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (!Physics.Raycast(ray, out hitInfo, Mathf.Infinity, groundLayer.value))
+            return;
+
+        if (Vector3.Distance(hitInfo.point, transform.position) < 1f)
+        {
+            return;
+        }
+
+        var lookPosition = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        
+        transform.LookAt(lookPosition);
+    }
+}
