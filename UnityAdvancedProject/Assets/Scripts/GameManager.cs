61d9e0c01d89775f1261f055bfc614358db65aaf
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-02-27 19:43:10 +0200

    character controller

--- UnityAdvancedProject/Assets/Scripts/GameManager.cs
+++ UnityAdvancedProject/Assets/Scripts/GameManager.cs
@@ -4,46 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private PlayerController playerController;
+    
     
     public static event Action OnGamePaused;
     public static event Action OnGameResumed;
 
     private bool _isGamePaused;
 
-    private void Awake()
-    {
-        if (playerController == null)
-        {
-            Debug.LogError("Game Manager is missing the player controller! ABORTING Awake!");
-            return;
-        }
-
-        playerController.OnPlayerPressedPause += HandleOnPlayerPressedPause;
-    }
-
-    private void OnDestroy()
-    {
-        if (playerController == null)
-        {
-            return;
-        }
-
-        playerController.OnPlayerPressedPause -= HandleOnPlayerPressedPause;
-    }
-
-    private void HandleOnPlayerPressedPause()
-    {
-        if (!_isGamePaused)
-        {
-            _isGamePaused = true;
-            Debug.Log($"Game is paused!");
-            OnGamePaused?.Invoke();
-            return;
-        }
-        
-        _isGamePaused = false;
-        Debug.Log($"Game is resumed!");
-        OnGameResumed?.Invoke();
-    }
+    
 }
